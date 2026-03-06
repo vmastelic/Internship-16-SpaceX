6323ee09befe7992da75d4a9746bf4470f257151
Vatroslav Mastelić <vmaste00@fesb.hr>
2026-03-06 13:05:21 +0100

    display 10 flights on page

--- src/pages/Launches/Launches.tsx
+++ src/pages/Launches/Launches.tsx
@@ -1,28 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "../../components/NavBar";
 import { getLaunches } from "../../api/launches";
+import type { LaunchesResponse } from "../../types/launch";
 
-function Launches(){
+function Launches() {
 
+    const [data, setData] = useState<LaunchesResponse | null>(null);
+    const [loading, setLoading] = useState(true);
+    
     useEffect(() => {
         const fetchLaunches = async () => {
             try {
-                const data = await getLaunches();
-                console.log(data.docs);
-            } catch (error) {
-                console.error('Error fetching launches:', error);
-            }
-        };
-
-        fetchLaunches();
-    }, []);
-
-    return(
+                const result = await getLaunches();
+                console.log(result.docs);
+                setData(result);
+              } catch (error) {
+                  console.error("Error fetching launches:", error);
+              } finally {
+                  setLoading(false);
+              }
+          };
+          
+          fetchLaunches();
+      }, []);
+      
+      if (loading) {
+          return <p>Loading...</p>;
+      }
+      
+      return (
         <div>
-            <NavBar/>
-            <h1>Launches</h1>
+          <NavBar />
+          <h1>Launches</h1>
+          <ul style={{listStyle: "none"}}>
+            {data?.docs.map((launch) => (
+              <li key={launch.id}>
+                <h2>{launch.name}</h2>
+                <p>Date: {new Date(launch.date_utc).toLocaleDateString()}</p>
+              </li>
+            ))}
+          </ul>
         </div>
-    )
+      );
 }
 
 export default Launches;
